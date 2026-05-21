3fc81cd53d70db42c39caa9f13201151b2703673
Forge <forge@aine.app>
2026-05-21 15:27:08 +0000

    feat: update main message to "What's poppin!"

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>What up, Royce?</p>
+        <p>What&apos;s poppin!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             Count is: {count}
@@ -40,4 +40,3 @@ function App() {
 }
 
 export default App;
-
