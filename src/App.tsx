18c7aaa7d72ef9484bb61b42a97c9075831e6889
Aine Forge <forge-agent@users.noreply.github.com>
2026-07-30 18:52:55 +0000

    feat: add interactive Michigan Cities map page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { CrosswordPage } from '@/pages/CrosswordPage';
 import { Game2048Page } from '@/pages/Game2048Page';
 import { HomePage } from '@/pages/HomePage';
 import { MattPage } from '@/pages/MattPage';
+import { MichiganCitiesPage } from '@/pages/MichiganCitiesPage';
 import { ShootingStarsPage } from '@/pages/ShootingStarsPage';
 import { SpreadsheetPage } from '@/pages/SpreadsheetPage';
 import { StateCapitalsPage } from '@/pages/StateCapitalsPage';
@@ -46,6 +47,8 @@ function App() {
         return <SpreadsheetPage />;
       case '/colorado-cities':
         return <ColoradoCitiesPage />;
+      case '/michigan-cities':
+        return <MichiganCitiesPage />;
       case '/matt':
         return <MattPage />;
       case '/contact':
