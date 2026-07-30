775561ea308582608438beaa19177a2c853aeffd
Forge (royceacho-wwt) <royceacho-wwt@users.noreply.github.com>
2026-07-30 16:42:49 +0000

    fix: escape apostrophes in MattPage banner text
    Replace bare apostrophes with &apos; to satisfy the
    react/no-unescaped-entities ESLint rule.

--- src/pages/MattPage.tsx
+++ src/pages/MattPage.tsx
@@ -5,8 +5,8 @@ export function MattPage() {
     <main className="matt-page">
       <div className="scrolling-banner-container">
         <div className="scrolling-banner">
-          <span className="banner-text">MATT'S FORGE MADE THIS.</span>
-          <span className="banner-text">MATT'S FORGE MADE THIS.</span>
+          <span className="banner-text">MATT&apos;S FORGE MADE THIS.</span>
+          <span className="banner-text">MATT&apos;S FORGE MADE THIS.</span>
         </div>
       </div>
     </main>
